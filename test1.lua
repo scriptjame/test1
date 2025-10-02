b22c02983bdbc12c2c527080fadbbe59d485703e
scriptjame <132070412+scriptjame@users.noreply.github.com>
2025-10-02 13:07:59 +0700

    Update test1.lua

--- test1.lua
+++ test1.lua
@@ -12,7 +12,7 @@ if old then old:Destroy() end
 
 -- ⚡ Chạy script chính trước
 pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptjame/trybb/refs/heads/main/tryV3.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptjame/BBall1/refs/heads/main/V3.lua"))()
         loadstring(game:HttpGet("https://paste.debian.net/plainh/1b143a24/", true))()
 end)
 
