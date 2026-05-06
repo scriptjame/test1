1abd54d88094d08eb9bf6611f851dfecf8ef15e8
scriptjame <132070412+scriptjame@users.noreply.github.com>
2026-05-06 15:11:47 +0700

    Update guimm2.lua

--- guimm2.lua
+++ guimm2.lua
@@ -130,7 +130,7 @@ TweenService:Create(btn,TweenInfo.new(.1),{Size=UDim2.new(0.92,0,0,32)}):Play()
 if copy then
 
 if setclipboard then
-setclipboard("www.tiktok.com/@renan1627")
+setclipboard("https://www.youtube.com/@Yong3469")
 end
 
 game.StarterGui:SetCore("SendNotification",{
@@ -172,7 +172,7 @@ layout.HorizontalAlignment="Center"
 
 local socials={
 {name="TikTok",color=Color3.fromRGB(255,0,128),link="www.tiktok.com/@renan1627"},
-{name="YouTube",color=Color3.fromRGB(255,0,0),link="https://www.youtube.com/@Dearax-f9v"},
+{name="YouTube",color=Color3.fromRGB(255,0,0),link="https://www.youtube.com/@Yong3469"},
 {name="Discord",color=Color3.fromRGB(114,137,218),link="https://discord.gg/RbhFbKbABe"}
 }
 
